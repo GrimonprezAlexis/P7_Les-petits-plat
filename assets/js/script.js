0766dd01f0dd39f21928bee5d4d2fb0d745ea9db
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-23 00:50:17 +0100

    Transform CSS to SCSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+
+// Show / Hide dropdown list
+const toggleDropdown = (showList, listElem) => {
+    // Icon is up if value is found
+    if(showList){
+        $("#icon-dropdown-down").css("display", "none");
+        $("#icon-dropdown-up").css("display", "inherit");        
+        $(`#${listElem} li`).css("display", "inherit");
+    } else {
+        // Default icon is down
+        $("#icon-dropdown-down").css("display", "inherit");
+        $("#icon-dropdown-up").css("display", "none");        
+        $(`#${listElem} li`).css("display", "none");
+    }
+}
+
+//Filter words grab by user rather than list of words
+//dropdownInputPrimary, dropdownPrimaryList
 const filterFunction = (elemInput, listElem) => {
     let input, filter, ul, li, a, i, txtValue;
     let foundValue = [];
@@ -6,9 +24,11 @@ const filterFunction = (elemInput, listElem) => {
     filter = input.value.toUpperCase();
 
     if(input.value.length >= 3){
-        $(listElem).css("display", "inherit");
+        //By default the list is hide
+        $(`#${listElem}`).css("display", "inherit");
         ul = document.getElementById(listElem);
         li = ul.getElementsByTagName("li");
+
         for (i = 0; i < li.length; i++) {
             a = li[i].getElementsByTagName("a")[0];
             txtValue = a.textContent || a.innerText;
@@ -19,20 +39,12 @@ const filterFunction = (elemInput, listElem) => {
                 foundValue.push(false);
                 li[i].style.display = "none";
             }
+            //When is finish
             if(i == li.length -1){
-                if(foundValue.includes(true)) {
-                    $("#icon-dropdown-down").css("display", "none");
-                    $("#icon-dropdown-up").css("display", "inherit");
-                } else {
-                    $("#icon-dropdown-down").css("display", "inherit");
-                    $("#icon-dropdown-up").css("display", "none");
-                }
+                toggleDropdown(foundValue.includes(true), listElem);
             }
         }
     } else {
-        $(listElem).css("display", "none");
-        $("#icon-dropdown-down").css("display", "inherit");
-        $("#icon-dropdown-up").css("display", "none");
+        toggleDropdown(false, listElem);
     }
-
 }

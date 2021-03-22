cf84e8850d6fd450dab3f7a3da911ff5f24e1f02
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-22 15:49:33 +0100

    import Boostrap SCSS 4.0, source file

---
+++ compile.bat
@@ -0,0 +1,2 @@
+REM Compile CSS without source map, style.css.map : Allows browser mapping between CSS and SCSS
+.\assets\css\dart-sass\sass assets/css/sass/style.scss style.css --style=compressed --no-source-map
---
+++ watch.bat
@@ -0,0 +1,2 @@
+REM Regenerates the style.css file on each modification
+.\assets\css\dart-sass\sass assets/css/sass/style.scss style.css --watch

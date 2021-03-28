da095edd8382cef221f28494c797d7d27563f11d
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-03-28 19:53:04 +0200

    Fix Aria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,154 @@
-    //Fetch data from json file
-    fetch('/data/recipes.json')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        appendRecipes(data);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+//Fetch data from json file
+fetch('/data/recipes.json')
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    appendAndMapRecipes(data);
+})
+.catch(err => {
+    console.log(err);
+});
 
-    let arrayOfRecipes = [];
+let arrayOfIngredients = [];
+let arrayOfAppliance = [];
+let arrayOfUstensils = [];
 
-    //Loop the list of recipes
-    appendRecipes = function appendRecipes(recipes) {
-        let recipesList = $("#recipes");
-        recipes.forEach(r => {
+//Loop the list of recipes
+const appendAndMapRecipes = (recipes) => {
+    let recipesList = $("#recipes");
+    let ingredientsList = $('#dropdownGreenList');
+    let appliancesList = $('#dropdownGreenList');
+    let ustensilsList = $('#dropdownOrangeList');
 
-            recipesList.append(`
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow no-border">
-                        <img class="card-img-top" src="/assets/img/bg-recipe.png" alt="Card image cap" />
-                        <div class="card-body card-body-style">
-                            <div class="d-flex justify-content-between">
-                                <label class="card-title">${r.name}</label>
-                                <div class="card-time"><img src="./assets/img/clock-icon.png" class="icon clockIcon" /> <span>${r.time} min</span></div>
-                            </div>
-                            <div class="row d-flex justify-content-between">
-                                <ul class="col-md-6 card-ingredients" id="id-card-ingredients-${r.id}"></ul>
-                                <small class="col-md-6 card-description">${r.description}</small>
-                            </div>
+    
+    recipes.forEach(r => {
+
+        //Init appareil list
+        if(arrayOfAppliance.indexOf(r.appliance) === -1){
+            arrayOfAppliance.push(r.appliance);
+        }
+
+        //Init ustanciles lists
+        r.ustensils.map((ustensil) => { 
+            if(!arrayOfUstensils.includes(ustensil)) arrayOfUstensils.push(ustensil); 
+        });
+
+        //Add recipes list
+        recipesList.append(`
+            <div class="col-md-4">
+                <div class="card mb-4 box-shadow no-border">
+                    <img class="card-img-top" src="/assets/img/bg-recipe.png" alt="Card image cap" />
+                    <div class="card-body card-body-style">
+                        <div class="d-flex justify-content-between">
+                            <label class="card-title">${r.name}</label>
+                            <div class="card-time"><img src="./assets/img/clock-icon.png" class="icon clockIcon" /> <span>${r.time} min</span></div>
+                        </div>
+                        <div class="row d-flex justify-content-between">
+                            <ul class="col-md-6 card-ingredients" id="id-card-ingredients-${r.id}"></ul>
+                            <small class="col-md-6 card-description">${r.description}</small>
                         </div>
                     </div>
                 </div>
+            </div>
+        `);
+
+        //Init list of ingredients
+        r.ingredients.forEach(i => {
+            if(arrayOfIngredients.indexOf(i.ingredient) === -1){
+                arrayOfIngredients.push(i.ingredient);
+            }
+
+            //Add ingredient associate to recipe ID
+            let cardIngredients = $(`#id-card-ingredients-${r.id}`);
+            cardIngredients.append(`
+                <li>${i.ingredient}: <span>${i.quantity ? i.quantity : ''} ${i.unit ? i.unit : ''}</span></li>
             `);
-
-            r.ingredients.forEach(i => {
-                let cardIngredients = $(`#id-card-ingredients-${r.id}`);
-                cardIngredients.append(`
-                    <li>${i.ingredient}: <span>${i.quantity ? i.quantity : ''} ${i.unit ? i.unit : ''}</span></li>
-                `);
-            });
-
         });
-    }
+    });
 
 
-// Show / Hide dropdown list
-const toggleDropdown = (showList, listElem) => {
+    //Add list of ingrédient
+    arrayOfIngredients.forEach(ingredient => {
+        ingredientsList.append(`
+            <li><a href="#">${ingredient}</a></li>
+        `)
+    });
+    //Add list of appliance (appareil)
+    arrayOfAppliance.forEach(appliance => {
+        appliancesList.append(`
+            <li><a href="#">${appliance}</a></li>
+        `)
+    });
+    //Add list of ustanciles
+    arrayOfUstensils.forEach(ustensil => {
+        ustensilsList.append(`
+            <li><a href="#">${ustensil}</a></li>
+        `)
+    });
+}
+
+const toggleDropdownList = (dropdownList) => {
+    $(`#${dropdownList}`).toggle();
+}
+
+
+// Toggle icon of input
+const toggleIconDropdown = (showList) => {
     // Icon is up if value is found
     if(showList){
         $("#icon-dropdown-down").css("display", "none");
         $("#icon-dropdown-up").css("display", "inherit");        
-        $(`#${listElem} li`).css("display", "inherit");
     } else {
         // Default icon is down
         $("#icon-dropdown-down").css("display", "inherit");
         $("#icon-dropdown-up").css("display", "none");        
-        $(`#${listElem} li`).css("display", "none");
     }
 }
 
+const filterFunction = (inputElem, dropdownList) => {
+    let input, filter, ul, li, a, i, txtValue;
+    let foundValue = [];
+
+    //"dropdownInputPrimary"
+    input = document.getElementById(inputElem);
+    filter = input.value.toUpperCase();
+
+    if(input.value.length >= 3){
+        $(`#${dropdownList}`).css("display", "inherit");
+        ul = document.getElementById(dropdownList);
+        li = ul.getElementsByTagName("li");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                foundValue.push(true);
+                li[i].style.display = "";
+            } else {
+                foundValue.push(false);
+                li[i].style.display = "none";
+            }
+            if(i == li.length -1){
+                if(foundValue.includes(true)) {
+                    toggleIconDropdown(foundValue.includes(true));
+                } else {
+                    toggleIconDropdown(false);
+                }
+            }
+        }
+    } else {
+        $(`#${dropdownList}`).css("display", "none");
+        toggleIconDropdown(false);
+    }
+
+}
+
+
+
 //Filter words grab by user rather than list of words
 //dropdownInputPrimary, dropdownPrimaryList
-const filterFunction = (elemInput, listElem) => {
+/* const filterFunction = (elemInput, listElem) => {
     let input, filter, ul, li, a, i, txtValue;
     let foundValue = [];
 
@@ -95,4 +179,4 @@ const filterFunction = (elemInput, listElem) => {
     } else {
         toggleDropdown(false, listElem);
     }
-}
+} */
